eab8d7f8bd8b717d23daf4710d6959bbc9e90631
IPSOSGROUP\soeren.spiegel <soeren.spiegel@ipsos.com>
2018-08-01 17:08:03 +0200

    Creation of the first RegistrationController and the registration html template

---
+++ src/main/java/de/hsba/test/bike/bike/web/RegistrationController.java
@@ -0,0 +1,12 @@
+package de.hsba.test.bike.bike.web;
+
+import de.hsba.test.bike.bike.user.User;
+import org.springframework.stereotype.Controller;
+
+
+
+@Controller
+public class RegistrationController {
+
+
+}
