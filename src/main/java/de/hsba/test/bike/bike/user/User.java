ebec6ae6b3d522eed18e7e3ba6f8a8888630438b
IPSOSGROUP\soeren.spiegel <soeren.spiegel@ipsos.com>
2018-08-01 13:39:37 +0200

    User class was created

---
+++ src/main/java/de/hsba/test/bike/bike/user/User.java
@@ -0,0 +1,69 @@
+package de.hsba.test.bike.bike.user;
+
+import jdk.jfr.Enabled;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public User(String name, String password, String role) {
+        this.name = name;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public String toString(){
+        return "User{" +
+                "name=" + name + '\'' +
+                '}';
+    }
+}
+
