caeb390a3efe53ef0aa6776b42f521972ab94406
denis semko <denis.semko@gmail.com>
2018-09-02 00:26:14 +0200

    implemented edit order function

---
+++ src/main/java/de/hsba/test/bike/bike/web/EditOrderController.java
@@ -0,0 +1,61 @@
+package de.hsba.test.bike.bike.web;
+
+import de.hsba.test.bike.bike.order.Order;
+import de.hsba.test.bike.bike.order.OrderService;
+import de.hsba.test.bike.bike.user.UserService;
+import de.hsba.test.bike.bike.web.exceptions.NotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/editorder/{id}")
+public class EditOrderController {
+
+    private final OrderService orderService;
+
+    private final UserService userService;
+
+    private final OrderFormAssembler formAssembler;
+
+    public EditOrderController(OrderService orderService, UserService userService, OrderFormAssembler formAssembler) {
+        this.orderService = orderService;
+        this.userService = userService;
+        this.formAssembler = formAssembler;
+    }
+
+    @ModelAttribute("order")
+    public Order findOrder(@PathVariable("id") Long id) {
+        Order order = orderService.findOrder(id);
+        if (order == null) {
+            throw new NotFoundException();
+        }
+        return order;
+    }
+
+    @GetMapping
+    public String show(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("orderForm", formAssembler.toForm(findOrder(id)));
+        return "editorder";
+    }
+
+    @PostMapping
+    public String change(Model model, @PathVariable("id") Long id,
+                         @ModelAttribute("orderForm") @Valid OrderForm orderForm, BindingResult orderBinding) {
+        if (orderBinding.hasErrors()) {
+            return "editorder";
+        }
+        orderService.save(formAssembler.update(findOrder(id), orderForm));
+        return "redirect:/customerOrder";
+    }
+
+    @PostMapping(path = "/cancel")
+    public String cancel(@PathVariable("id") Long id) {
+
+       return "redirect:/customerOrder";
+    }
+}
