2cccfa118267620b884fcf5ca2a694c1e96f5479
Denis Semko <denis.semkoandrosenko@myhsba.de>
2018-09-07 15:23:03 +0200

    server side validation / security for editOrder

---
+++ src/main/java/de/hsba/test/bike/bike/web/exceptions/ConflictException.java
@@ -0,0 +1,9 @@
+package de.hsba.test.bike.bike.web.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class ConflictException extends RuntimeException {
+    public Long id;
+}
